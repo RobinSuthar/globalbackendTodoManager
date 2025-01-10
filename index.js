52bd6fc20e9100e888481203b5a1b979eb08d6a6
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-10 13:00:53 -0700

    Some Core logic and been adde beforer seting up db

--- index.js
+++ index.js
@@ -4,17 +4,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT;
-app.get("/GlobalTodos", function (req, res) {
+app.get("/GlobalTodos/AllTodos", function (req, res) {
+  //some logic to retrive all todos which type is global
   res.json({ msg: "Wokring on the logic" });
 });
 
-app.get("/GlobalTodos", function (req, res) {
+app.post("/GlobalTodos/CreateTodo", function (req, res) {
+  const [title, description] = req.body;
+  const name = "GlobalUser";
+
+  //Some Logic To add the new todo into data
   res.json({ msg: "Wokring on the logic" });
 });
 
-app.get("/GlobalTodos", function (req, res) {
+app.put("/GlobalTodos/Completed", function (req, res) {
+  const id = req.body.id;
+
+  //Some Logic To Update the status of the todo to be completed.
+  res.json({ msg: "Wokring on the logic" });
+});
+
+app.put("/GlobalTodos/NotCompleted", function (req, res) {
+  const id = req.body.id;
+
+  //Some Logic To Update the status of the todo to be completed.
   res.json({ msg: "Wokring on the logic" });
 });
 

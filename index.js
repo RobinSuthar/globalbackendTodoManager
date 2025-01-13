e17f8336fec63d8439ab8f2f617cddb6f1dd87fd
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-12 23:25:42 -0700

    Working on the company logic

--- index.js
+++ index.js
@@ -222,6 +222,7 @@ app.put("/Indiviualtodos/NotCompleted", async function (req, res) {
   res.json({ msg: "Todo Has been updated to NotCompleted in Db" });
 });
 
+//********************** */
 //End point for userDatbase
 
 app.post("/users/CreateUser", async function (req, res) {
@@ -283,6 +284,107 @@ app.post("/Organzations/CreateOrganztion", async function (req, res) {
 
   //Some Logic To Update the status of the todo to be completed.
   res.json({ msg: "Organztiopn has been added to Database" });
+});
+
+//Three End point for the Company Todos Management
+
+app.get("/GlobalTodos/AllTodos", async function (req, res) {
+  //some logic to retrive all todos which type is global
+  const allTodosWithGlobalType = await TodoDatabase.find({ type: "Global" });
+  console.log(allTodosWithGlobalType);
+  if (!allTodosWithGlobalType) {
+    res.status(401).json({
+      msg: "Cannot get the Todos with Global type",
+    });
+  }
+  res.json({ allTodosWithGlobalType });
+});
+
+app.post("/GlobalTodos/CreateTodo", async function (req, res) {
+  const { title, description } = req.body;
+  const ParsedUserInputs = TodoSchema.safeParse(req.body);
+  if (!ParsedUserInputs.success) {
+    return res.json({
+      msg: "Invalid Title or Description length",
+    });
+  }
+
+  const CreatingTodo = await TodoDatabase.create({
+    type: "Global",
+    title: title,
+    description: description,
+    username: "Global",
+    isCompleted: false,
+  });
+
+  if (!CreatingTodo) {
+    res.status(401).json({
+      msg: "UnFortunately Cannot add into DataBase",
+    });
+    return;
+  }
+  res.json({ msg: "Todo has been SuccessFully added to DataBase" });
+});
+
+app.put("/GlobalTodos/Completed", async function (req, res) {
+  const updateTodoId = req.body.id;
+  const Parsingid = UserId.safeParse(updateTodoId);
+  if (!Parsingid.success) {
+    return res.json({
+      msg: "Incorrect Id",
+    });
+  }
+  try {
+    const UpdateingTodoToCompleted = await TodoDatabase.findOneAndUpdate(
+      { _id: updateTodoId },
+      {
+        isCompleted: true,
+      }
+    );
+    if (!UpdateingTodoToCompleted) {
+      return res.status(401).json({
+        msg: "Unable to Update Todo To Completed ",
+      });
+    }
+  } catch (err) {
+    return res.json({ msg: err });
+  }
+
+  //Some Logic To Update the status of the todo to be completed.
+  res.json({ msg: "Todo Has been updated To Completed in Db" });
+});
+
+app.put("/GlobalTodos/NotCompleted", async function (req, res) {
+  const updateTodoId = req.body.id;
+
+  const Parsingid = UserId.safeParse(updateTodoId);
+
+  if (!Parsingid.success) {
+    return res.json({
+      msg: "Incorrect Id",
+    });
+  }
+
+  try {
+    const UpdateingTodoToCompleted = await TodoDatabase.findOneAndUpdate(
+      { _id: updateTodoId },
+      {
+        isCompleted: false,
+      }
+    );
+    if (!UpdateingTodoToCompleted) {
+      res.status(401).json({
+        msg: "Unable to Update Todo To Completed ",
+      });
+    }
+  } catch (err) {
+    return res.json({
+      msg: err,
+    });
+  }
+
+  //Some Logic To Update the status of the todo to be completed.
+  res.json({ msg: "Todo Has been updated to NotCompleted in Db" });
 });
 
 app.listen(PORT, function () {

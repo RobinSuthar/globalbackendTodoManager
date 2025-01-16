5165b7382d9be257cca822f17ad10cd4c6cb21e4
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-16 15:17:04 -0700

    Changes in Schema so long title and descr cannot be added

--- type.js
+++ type.js
@@ -1,29 +1,29 @@
 import zod, { string } from "zod";
 
 const TodoSchema = zod.object({
-  title: zod.string().min(2).max(40),
-  description: zod.string().min(2).max(40),
+  title: zod.string().min(2).max(20),
+  description: zod.string().min(2).max(25),
 });
 
 const TodoSchemaIndiviual = zod.object({
-  title: zod.string().min(2).max(40),
-  description: zod.string().min(1).max(40),
-  username: zod.string().min(1).max(20),
+  title: zod.string().min(2).max(20),
+  description: zod.string().min(1).max(25),
+  username: zod.string().min(1).max(15),
 });
 
 const OrganizationSchemaValdaition = zod.object({
-  companyName: zod.string().min(5).max(40),
+  companyName: zod.string().min(5).max(20),
   companyPin: zod.string().min(2).max(15),
 });
 
 const CompanySchema = zod.object({
   companyName: zod.string(),
-  author: zod.string().min(1).max(40),
-  title: zod.string().min(1).max(40),
-  description: zod.string().min(1).max(40),
+  author: zod.string().min(1).max(20),
+  title: zod.string().min(1).max(20),
+  description: zod.string().min(1).max(25),
 });
 
-const UserNameSchema = zod.string().min(2).max(30);
+const UserNameSchema = zod.string().min(2).max(20);
 
 const UserId = zod.string().min(5).max(50);
 export {
